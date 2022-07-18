9b20d6ac2097c56d00d44f1bd39f6da99a4eeeea
Lucas Côrte <lucascorte@outlook.com>
2022-07-18 19:40:00 -0300

    DTO

--- backend/src/main/java/com/lucasprojects/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/lucasprojects/dscatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.lucasprojects.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lucasprojects.dscatalog.entities.Category;
+import com.lucasprojects.dscatalog.entities.dtos.CategoryDTO;
 import com.lucasprojects.dscatalog.repositories.CategoryRepository;
 
 @Service
@@ -14,7 +16,9 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 
-	public List<Category> findAll() {
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		
+		return list.stream().map(category -> new CategoryDTO(category)).collect(Collectors.toList());
 	}
 }
