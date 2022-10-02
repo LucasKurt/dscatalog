20c456ecb7ac591fbe9bcb19d348b599b9b48a30
Lucas Côrte <lucascorte@outlook.com>
2022-10-02 20:24:18 -0300

    Implementing oauth2 authorization server

--- backend/src/main/java/com/lucasprojects/dscatalog/config/SecurityConfig.java
+++ backend/src/main/java/com/lucasprojects/dscatalog/config/SecurityConfig.java
@@ -2,22 +2,46 @@ package com.lucasprojects.dscatalog.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
 	@Bean
-	public SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
-		return httpSecurity
-				.headers(headers -> headers.frameOptions().disable())			
-				.csrf(csrf -> csrf.disable())
-				.authorizeHttpRequests(auth -> {
-					auth.antMatchers("/**").permitAll();
-				})
-				.build();
+	BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
+
+	@Bean
+	JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+		tokenConverter.setSigningKey("MY-JWT-SECRET");
+		return tokenConverter;
+	}
+	
+	@Bean
+	JwtTokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
+	}
+	
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+//	@Bean
+//	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+//		return http
+//				.headers(headers -> headers.frameOptions().disable())
+//				.csrf(csrf -> csrf.disable())
+//				.authorizeHttpRequests(auth -> auth.antMatchers("/actuator/**").permitAll())
+//				.build();
+//	}
 }
--- backend/src/main/java/com/lucasprojects/dscatalog/services/UserService.java
+++ backend/src/main/java/com/lucasprojects/dscatalog/services/UserService.java
@@ -4,11 +4,16 @@ import java.util.Optional;
 
 import javax.persistence.EntityNotFoundException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,10 +26,12 @@ import com.lucasprojects.dscatalog.repositories.RoleRepository;
 import com.lucasprojects.dscatalog.repositories.UserRepository;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
+	
 	@Autowired
-	private BCryptPasswordEncoder passwordEncoder;
+	private BCryptPasswordEncoder passwordEncoder; 
 	
 	@Autowired
 	private UserRepository repository;
@@ -96,5 +103,17 @@ public class UserService {
 				throw new EntityNotFoundException("Unable to find category with id " + roleDto.getId());
 			}
 		});
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {		
+		User user = repository.findByEmail(username);
+		if (user == null) {
+			logger.error("User not found: " + username);
+			throw new UsernameNotFoundException("User not found");
+		}
+		
+		logger.info("User found: " + username);
+		return user;
 	}
 }
